4483919564fb9db1ced08d1a77e53dc41cdfd7b5
Wilmer Lin <unitygameacademy@gmail.com>
2020-06-03 09:47:07 -0700

    added RemoveOnDeathSystem

---
+++ Assets/Scripts/Systems/RemoveOnDeathSystem.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+[UpdateBefore(typeof(TransformSystemGroup))]
+public class RemoveOnDeathSystem : SystemBase
+{
+    private EndSimulationEntityCommandBufferSystem commandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        commandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+    protected override void OnUpdate()
+    {
+        EntityCommandBuffer entityCommandBuffer = commandBufferSystem.CreateCommandBuffer();
+
+        Entities.
+            WithAny<PlayerTag,ChaserTag>().
+            ForEach((Entity entity, in HealthData healthData) =>
+            {
+                if (healthData.isDead)
+                {
+                    entityCommandBuffer.DestroyEntity(entity);
+                }
+
+            }).Schedule();
+
+        commandBufferSystem.AddJobHandleForProducer(this.Dependency);
+    }
+}
