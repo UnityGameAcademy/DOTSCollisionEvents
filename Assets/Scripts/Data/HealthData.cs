8cff69a4e5556b9922238b6f04ffce3518148dce
Wilmer Lin <unitygameacademy@gmail.com>
2020-06-03 07:22:48 -0700

    added HealthData Component

---
+++ Assets/Scripts/Data/HealthData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct HealthData : IComponentData
+{
+    public int currentValue;
+    public bool isDead;
+    
+}
